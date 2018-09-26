2424c9898eb2bbfc66da5a4f3c89dc79d7c6f1be
196bot <32585934+196bot@users.noreply.github.com>
2018-09-25 21:17:44 -0500

    Add HW3

---
+++ hw3.py
@@ -0,0 +1,75 @@
+"""
+matrix_multiply
+
+Given two 2-D input arrays `arr0`, `arr1`, return the matrix product arr0 * arr1.
+Return None if the matrix product does not exist.
+
+As with math, assume that indices are in [row][column] format, so each inner list is a row.
+"""
+def matrix_multiply(arr0, arr1):
+	pass
+
+"""
+nth_largest_element
+
+Given an input list `arr`, and index `n`, return the nth largest element.
+Avoid using built-in sorting methods.
+"""
+def nth_largest_element(arr, n):
+	pass
+
+"""
+reverse_block
+
+Given an input list `arr`, and a block size `n` > 0, reverse the list in blocks of n.
+
+Example:
+	Arguments:
+		[1,2,3, 4,5,6, 7], 3
+	Return:
+		[3,2,1, 6,5,4, 7]
+	(spacing added for emphasis)
+
+"""
+def reverse_block(arr, n):
+	pass
+
+"""
+subset_sum
+
+Given an input list `arr`, and a number `target`, return whether or not any possible subset of the values in `arr` could sum to `target`.
+
+Example 1:
+	Arguments:
+		[1,2,3,4,5,7], 13
+		7 + 4 + 2 = 13
+	Return:
+		True
+
+Example 2:
+	Arguments:
+		[1,2,-1,5,4,-196], 196
+	Return:
+		False
+"""
+def subset_sum(arr, target):
+	pass
+
+"""
+spiral_matrix
+
+Given an input 2-D array, return a list with the values obtained by following a clockwise spiral path, starting from [0][0], then proceeding to [0][n], [m][n], [m][0], then going inwards:
+
+Example:
+	Argument:
+		[[a,b,c,d,e],
+		 [f,g,h,i,j],
+		 [k,l,m,n,o],
+		 [p,q,r,s,t],
+		 [u,v,w,x,y]]
+	Return:
+		[a,b,c,d,e, j,o,t,y, x,w,v,u, p,k,f, g,h,i, n,s, r,q, l, m]
+"""
+def spiral_matrix(arr):
+	pass
+
